827dbf3c875f971b8eb69617a8b70e69f9a98899
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-09-20 03:01:43 -0300

    feat: add models

--- src/models/Administrator.ts
+++ src/models/Administrator.ts
@@ -3,8 +3,8 @@ import User from './User';
 export class Administrator extends User {
   isAdmin: boolean;
 
-  constructor(name: string, birthday: Date, email:string, phone: string) {
-    super(name, birthday, email, phone)
-    this.isAdmin = true; 
+  constructor(name: string, birthday: Date, email: string, phone: string) {
+    super(name, birthday, email, phone);
+    this.isAdmin = true;
   }
 }
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,25 +1,24 @@
 import { v4 as uuidv4 } from 'uuid';
 
 class Task {
-    id: string;
-    created_at: Date;
-    initial_date: Date;
-    end_date: Date;
-    initial_coordinate: string;
-    end_coordinate: string
-    observation: string;
-    description: string;
+  id: string;
+  created_at: Date;
+  initial_date: Date;
+  end_date: Date;
+  initial_coordinate: string;
+  end_coordinate: string;
+  observation: string;
+  description: string;
 
-    constructor(id: string, created_at: Date, initial_date: Date, end_date: Date, 
-        initial_coordinate: string, end_coordinate: string,  observation: string, description: string) {
-            
-        this.id = id;
-        this.created_at = created_at;
-        this.initial_date = initial_date;
-        this.end_date = end_date;
-        this.initial_coordinate = initial_coordinate;
-        this.end_coordinate = end_coordinate;
-        this.observation = observation;
-        this.description = description;
-      }
+  constructor(created_at: Date, initial_date: Date, end_date: Date, initial_coordinate: string, end_coordinate: string, observation: string, description: string) {
+    
+    this.id = uuidv4()
+    this.created_at = created_at;
+    this.initial_date = initial_date;
+    this.end_date = end_date;
+    this.initial_coordinate = initial_coordinate;
+    this.end_coordinate = end_coordinate;
+    this.observation = observation;
+    this.description = description;
+  }
 }
