ad4d99566ffb2a4418c0e8095e3bc876825473a7
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-10-20 21:46:37 -0300

    feat: domainn exeption validation and initial user entity

---
+++ src/HigiaServer.Domain/Commons/User.cs
@@ -0,0 +1,28 @@
+﻿namespace HigiaServer.Domain.Entities;
+
+abstract public class User
+{
+    public Guid Id { get; init; }
+    public bool IsAdmin { get; init; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Adress { get; private set; }
+    public DateTime Birthday { get; private set; }
+    public int Number { get; private set; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdateAt { get; private set; }
+
+    public User(Guid id, string firstName, string lastName, string adress, DateTime birthday, int number, bool isAdmin, DateTime createdAt, DateTime updateAt)
+    {
+        Id = Guid.NewGuid();
+        FirstName = firstName;
+        LastName = lastName;
+        Adress = adress;
+        Birthday = birthday;
+        Number = number;
+        IsAdmin = isAdmin;
+        CreatedAt = createdAt;
+        UpdateAt = updateAt;
+    }
+}
--- src/HigiaServer.Infra/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HigiaServer.Infra;
-
-public class Class1
-{
-
-}
