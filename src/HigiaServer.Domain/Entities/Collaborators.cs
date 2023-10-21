67346d3be8ecc1853c3693e4de4509fcf7aca014
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-10-21 02:29:13 -0300

    feat: created collaborator and administrator entities

--- src/HigiaServer.Domain/Entities/Collaborators.cs
+++ src/HigiaServer.Domain/Entities/Collaborators.cs
@@ -1,6 +1,10 @@
+using HigiaServer.Domain.Common;
+
 namespace HigiaServer.Domain.Entities;
 
-public class Collaborators
+public class Collaborators : BaseUserEntity
 {
-    
+    public Collaborators(string firstName, string lastName, string address, int number, DateTime? birthday, Administrator? lastModifiedBy, 
+Administrator? createdBy) : base(firstName, lastName, address, number, birthday, lastModifiedBy, createdBy)
+        => IsAdmin = false;
 }
