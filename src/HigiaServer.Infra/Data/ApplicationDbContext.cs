247428b9a70e7c59896e854a43c8e4fcb040f5f1
Ryan Gabriel <urltapas@pop-os.localdomain>
2023-11-08 21:29:26 -0300

    fix: dependency injection db context

--- src/HigiaServer.Infra/Data/ApplicationDbContext.cs
+++ src/HigiaServer.Infra/Data/ApplicationDbContext.cs
@@ -7,15 +7,11 @@ public class ApplicationDbContext : DbContext
     public DbSet<BaseUserEntity> Users { get; set; }
     public DbSet<Task> Tasks { get; set; }
 
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new BaseUserMap());
         modelBuilder.ApplyConfiguration(new TaskMap());
-    }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite("DataSource=application.db;Cache=Shared");
-        base.OnConfiguring(optionsBuilder);
     }
 }
--- src/HigiaServer.Infra/DependencyInjection.cs
+++ src/HigiaServer.Infra/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using HigiaServer.Application.MappingDTO;
+﻿using HigiaServer.Application.Interfaces;
+using HigiaServer.Application.MappingDTO;
+using HigiaServer.Application.Services;
+using HigiaServer.Infra.Data;
 
 namespace HigiaServer.Infra;
 
@@ -6,10 +9,18 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlite("DataSource=application.db;Cache=Shared"));
+
         services.AddAutoMapper(typeof(DomainDTOMapping));
+
         services.AddScoped<ICollaboratorRepository, CollaboratorRepository>();
         services.AddScoped<IAdministratorRepository, AdminsistratorRepository>();
         services.AddScoped<ITaskRepository, TaskRepository>();
+
+        services.AddScoped<ICollaboratorService, CollaboratorService>();
+        services.AddScoped<IAdministratorService, AdministratorService>();
+        services.AddScoped<ITaskService, TaskService>();
 
         return services;
     }
