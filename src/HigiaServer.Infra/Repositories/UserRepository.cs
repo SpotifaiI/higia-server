5878b2edc867b441905cc7dee6281515882e734e
Ryan Gabriel <ryan.bromati@hotmail.com>
2024-03-27 01:06:46 -0300

    feat: IUserRepository and UserRepository implementations

---
+++ src/HigiaServer.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+using HigiaServer.Application.Repositories;
+using HigiaServer.Domain.Entities;
+namespace HigiaServer.Infra.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private static readonly List<User> _users = [];
+    public void AddUser(User user) => _users.Add(user);
+    public User? GetUserByEmail(string email) => _users.SingleOrDefault(x => x.Email == email)!;
+    
+}
