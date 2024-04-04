98ef9f08b8b5f62eb8cfbc8024b9fdeaca8af29d
urltapas <ryan.bromati@hotmail.com>
2024-04-04 01:29:19 -0300

    feat: update register request and User classes to allow nullable number field

--- src/HigiaServer.Application/Contracts/Requests/RegisterRequest.cs
+++ src/HigiaServer.Application/Contracts/Requests/RegisterRequest.cs
@@ -2,7 +2,7 @@ namespace HigiaServer.Application.Contracts.Requests;
 
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-public class RegisterRequest(string email, string password, string name, bool isAdmin, string number)
+public class RegisterRequest(string email, string password, string name, bool isAdmin, string? number)
 {
     [EmailAddress]
     public string Email { get; private set; } = email;
@@ -10,5 +10,5 @@ public class RegisterRequest(string email, string password, string name, bool is
     public string Password { get; private set; } = password;
     public string Name { get; private set; } = name;
     public bool IsAdmin { get; private set; } = isAdmin;
-    public string Number { get; private set; } = number;
+    public string? Number { get; private set; } = number;
 }
