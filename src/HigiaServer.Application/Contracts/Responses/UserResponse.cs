2d34cdb7fcb7a6a5dbbc0cda1ef63db8cad83bdf
urltapas <ryan.bromati@hotmail.com>
2024-04-04 22:58:19 -0300

    refactor: userresponse

--- src/HigiaServer.Application/Contracts/Responses/UserResponse.cs
+++ src/HigiaServer.Application/Contracts/Responses/UserResponse.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HigiaServer.Application.Contracts.Responses;
 
-public record UserResponse(
-    string Email, 
-    string Name, 
-    bool IsAdmin
-);
+public class UserResponse(string email, string name, bool isAdmin)
+{
+    public string Name { get; private set; } = name;
+    [EmailAddress]
+    public string Email { get; private set; } = email;
+    public bool IsAdmin { get; private set; } = isAdmin;
+}
