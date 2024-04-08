012c62f8c61fb6a01f552b1890c027870cb4bf6d
urltapas <ryan.bromati@hotmail.com>
2024-04-08 20:48:04 -0300

    refactor: task and user classes

--- src/HigiaServer.Domain/Entities/Tasks.cs
+++ src/HigiaServer.Domain/Entities/Tasks.cs
@@ -10,9 +10,9 @@ public class Task(
     string? description)
 {
     public string Title { get; private set; } = title;
-    public string[] Coordinates { get; private set; } = coordinates;
     public UrgencyLevel UrgencyLevel { get; private set; } = urgencyLevel;
-    public List<User> Collaborators { get; private set; } = collaborators;
     public string? Description { get; private set; } = description;
+    public string[] Coordinates { get; private set; } = coordinates;
     public Status Status { get; private set; } = Status.New;
+    public List<User> Collaborators { get; private set; } = collaborators;
 }
--- src/HigiaServer.Domain/Entities/User.cs
+++ src/HigiaServer.Domain/Entities/User.cs
@@ -3,10 +3,10 @@ namespace HigiaServer.Domain.Entities;
 public class User(bool isAdmin, string name, string email, string password, string? number)
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public bool IsAdmin { get; private set; } = isAdmin;
-    public string Email { get; private set; } = email;
     public string Name { get; private set; } = name;
-    public string? Number { get; private set; } = number;
+    public string Email { get; private set; } = email;
     public string Password { get; private set; } = password;
+    public bool IsAdmin { get; private set; } = isAdmin;
+    public string? Number { get; private set; } = number;
     public List<Task>? Tasks { get; private set; } = isAdmin ? [] : null;
 }
