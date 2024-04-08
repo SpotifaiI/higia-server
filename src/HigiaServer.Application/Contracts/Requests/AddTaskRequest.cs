0a9c9ebfb9919acbfc2ec2c226425b284bde5b2b
urltapas <ryan.bromati@hotmail.com>
2024-04-08 20:48:28 -0300

    feat: add addtaskrequest class for handling task creation

---
+++ src/HigiaServer.Application/Contracts/Requests/AddTaskRequest.cs
@@ -0,0 +1,19 @@
+using HigiaServer.Domain.Enums;
+
+namespace HigiaServer.Application.Contracts.Requests;
+
+public class AddTaskRequest(
+    string title, 
+    UrgencyLevel urgencyLevel, 
+    string description, 
+    Coordinates coordinates, 
+    List<Guid> collaborators)
+{
+    public string Title { get; set; } = title;
+    public UrgencyLevel UrgencyLevel { get; set; } = urgencyLevel;
+    public string Description { get; set; } = description;
+    public Coordinates Coordinates { get; set; } = coordinates;
+    public List<Guid> Collaborators { get; set; } = collaborators;
+}
+
+public record Coordinates(string Latitude, string Longitude);
