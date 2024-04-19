57e9336ae9b7bfb5699d55fd3a45c77e60e5300e
urltapas <ryan.bromati@hotmail.com>
2024-04-19 19:35:42 -0300

    feat: add task mapping o on ico

--- src/HigiaServer.Infra/DependencyInjection.cs
+++ src/HigiaServer.Infra/DependencyInjection.cs
@@ -4,7 +4,6 @@ using HigiaServer.Application.Services;
 using HigiaServer.Infra.Configurations;
 using HigiaServer.Infra.Repositories;
 using HigiaServer.Infra.Services;
-
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +14,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfra(this IServiceCollection services, ConfigurationManager configuration)
     {
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddAutoMapper(typeof(TaskMapping));
         services.AddAutoMapper(typeof(AuthenticationMapping));
 
         services.AddScoped<IJwtTokenService, JwtTokenService>();
