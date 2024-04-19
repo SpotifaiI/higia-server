eec1c124515f4dc600b6df90f1e361db917c0e3f
urltapas <ryan.bromati@hotmail.com>
2024-04-19 19:33:39 -0300

    feat: update auth mapping

--- src/HigiaServer.Application/Mappers/AuthenticationMapping.cs
+++ src/HigiaServer.Application/Mappers/AuthenticationMapping.cs
@@ -1,5 +1,4 @@
 using AutoMapper;
-
 using HigiaServer.Application.Contracts.Requests;
 using HigiaServer.Application.Contracts.Responses;
 using HigiaServer.Domain.Entities;
@@ -13,14 +12,15 @@ public class AuthenticationMapping : Profile
         CreateMap<RegisterRequest, User>()
             .ConstructUsing(request => new User(
                 request.IsAdmin,
-                request.Email,
                 request.Name,
+                request.Email,
                 request.Password,
                 null
             ));
 
         CreateMap<User, UserResponse>()
             .ConstructUsing(user => new UserResponse(
+                user.Id,
                 user.Email,
                 user.Name,
                 user.IsAdmin
