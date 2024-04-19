7c379f7267714ac0cc51d1ce2ede39f03c96f52e
urltapas <ryan.bromati@hotmail.com>
2024-04-19 19:37:29 -0300

    deleted: UnauthenticatedUserException.cs

--- src/HigiaServer.Application/Errors/UnauthenticatedUserException.cs
+++
@@ -1,8 +0,0 @@
-using System.Net;
-
-namespace HigiaServer.Application.Errors;
-public class UnauthenticatedUserException : Exception, IServiceException
-{
-    public HttpStatusCode StatusCode => HttpStatusCode.Unauthorized;
-    public string ErrorMessage => "Access Denied. Authentication required for access.";
-}
