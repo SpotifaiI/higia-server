3f7b238bd75c6c4eb2c5624183910c1b05537e56
urltapas <ryan.bromati@hotmail.com>
2024-04-19 19:33:56 -0300

    feat: add roles on jwtsettigs

--- src/HigiaServer.Infra/Services/JwtTokenService.cs
+++ src/HigiaServer.Infra/Services/JwtTokenService.cs
@@ -1,11 +1,9 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-
 using HigiaServer.Application.Services;
 using HigiaServer.Domain.Entities;
 using HigiaServer.Infra.Configurations;
-
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -27,7 +25,8 @@ public class JwtTokenService(IOptions<JwtSettings> jwtSettings) : IJwtTokenServi
         [
             new Claim(JwtRegisteredClaimNames.Name, user.Id.ToString()),
             new Claim(JwtRegisteredClaimNames.Email, user.Email),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Role, user.IsAdmin ? "admin" : "collaborator")
         ];
 
         JwtSecurityToken securityToken =
