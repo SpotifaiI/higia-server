fd5d43babc606cbfb4d708c20e1fb97e09b9a64d
urltapas <ryan.bromati@hotmail.com>
2024-04-20 16:32:34 -0300

    #

---
+++ src/HigiaServer.Application/Errors/AddTaskCollaboratorNotFoundException.cs
@@ -0,0 +1,12 @@
+using System.Net;
+using HigiaServer.Application.Services;
+
+namespace HigiaServer.Application.Errors;
+
+public class AddTaskCollaboratorNotFoundException(string collaboratorId) : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+    public string ErrorMessage =>
+        $"The Collaborator with id {collaboratorId} was not found in the system. Please provide a valid Id.";
+}
